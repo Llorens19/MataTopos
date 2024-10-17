504f786405f489a4f102e20337483db6d1610963
Llorens19 <llorenssorianodiego@gmail.com>
2024-10-17 16:55:22 +0200

    Sale topo nuevo al clicar

--- frontend/views/game/game.controller.js
+++ frontend/views/game/game.controller.js
@@ -1,34 +1,39 @@
 
 const startGame = () => {
     hideAllMoles();
+    document.getElementById('play').style.display = 'none';
+    randomMole();
 
 
 
 }
-
-
 
 const buttons = () => {
     document.getElementById('play').addEventListener('click', startGame);
 }
 
 
+
+
 const showMole = (idMole) => {
     const img = document.getElementById(`topo_img_${idMole}`);
     img.src = '../../assets/img/saliendo.png'
+    img.classList.add('visible');
 
     setTimeout(() => {
         img.src = '../../assets/img/topo.png';
-        img.classList.add('visible;');
+
+        moleClick();
     }, 50);
 }
 
 const hideMole = (idMole) => {
     const img = document.getElementById(`topo_img_${idMole}`);
     img.src = '../../assets/img/saliendo.png';
+    img.classList.remove('visible');
     setTimeout(() => {
         img.src = '../../assets/img/agujero.png';
-        img.classList.remove('visible');
+
     }, 50);
 }
 
@@ -46,8 +51,33 @@ const hideAllMoles = () => {
 }
 
 
+const randomMole = () => {
+    const moleId = Math.floor(Math.random() * 5) + 1;
+    showMole(moleId);
 
 
+}
+
+const moleClick = () => {
+
+    const visibleMoles = document.querySelectorAll('.visible');
+    visibleMoles.forEach(mole => {
+        mole.replaceWith(mole.cloneNode(true));
+    });
+
+
+
+    document.getElementsByClassName('visible')[0].addEventListener('click', (event) => {
+
+        console.log(event.target.id);
+
+        const id = event.target.id.split('_')[2];
+        hideMole(id);
+        setTimeout(() => {
+            randomMole();
+        }, 1000);
+    });
+}
 
 
 
