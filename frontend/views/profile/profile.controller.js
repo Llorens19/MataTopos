a7b45fd9653ffe16385b9a3658cf8c9a196659b8
Llorens19 <llorenssorianodiego@gmail.com>
2024-11-10 17:15:50 +0100

    Modificación del profile

--- frontend/views/profile/profile.controller.js
+++ frontend/views/profile/profile.controller.js
@@ -4,6 +4,20 @@ const loadProfile = () => {
     document.getElementById('username').innerText = data.username;
     document.getElementById('img_profile').src = data.image;
     document.getElementById('email').innerText = data.email;
+    document.getElementById('points').innerText = data.points;
+    document.getElementById('coins').innerText = data.coins;
+}
+
+const loadSkins = () => {
+    const data = JSON.parse(localStorage.getItem('user'));
+    const skins = data.skins;
+
+    skins.unshift(0);
+
+    let html_skins = skins.map((skin) => {
+        return `<img src="../../assets/img/topo_${skin}.png" class = "img_skin"/>`
+    }).join('');
+    document.getElementById('skins').innerHTML += html_skins;
 }
 
 
@@ -11,5 +25,6 @@ const loadProfile = () => {
 
 document.addEventListener('DOMContentLoaded', (event) => {
     loadProfile();
+    loadSkins();
 
 });
